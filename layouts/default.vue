7a4f82212fe9e60a3e3dcd28c9fbcf13e88ee33f
Serhiy <serhiybilik@gmail.com>
2021-05-10 16:25:06 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+input {
+  box-shadow: none;
+}
+a {
+  text-decoration: none;
+  color: #7ab257;
+}
+.container {
+  max-width: 1200px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+</style>
